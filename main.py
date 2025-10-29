9f3bc7dea37d78d1236241b30d7344e72ed3ed69
slavykov <slavykpota2@gmail.com>
2025-10-29 11:36:54 +0200

    Refactor line drawing logic for PDF generation to improve readability

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,20,200,20)
+
+    for y in range(20,290,10):
+        pdf.line(10,y,200,y)
 
     # Set the footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0,h=10,txt=row["Topic"], align="R")
-
+    
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(20,290,10):
+            pdf.line(10,y,200,y)
         # Set the footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
